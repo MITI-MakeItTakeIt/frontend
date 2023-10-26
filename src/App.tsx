cdd6e463c2fab519632b51577aaf0f417b65c2b5
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-26 21:11:36 +0900

    ✨feat : react router dom 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Header } from "./components/Header";
-import { MainPage } from "./pages/MainPage";
+// import { Outlet } from "react-router-dom";
+// import { Header } from "./components/Header";
+// import { MainPage } from "./pages/MainPage";
 
 function App() {
   return (
     <>
-      <Header />
-      <MainPage />
+      {/* <Header /> */}
+      {/* <Outlet /> */}
+      {/* <MainPage /> */}
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { MainPage } from "./pages/MainPage.tsx";
+import { UserLogin } from "./pages/UserLogin.tsx";
+import { UserRegister } from "./pages/UserRegister.tsx";
+import { GamesList } from "./pages/GamesList.tsx";
+import { GameOperate } from "./pages/GameOperate.tsx";
+import { NotFoundPage } from "./pages/NotFoundPage.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Layout } from "./pages/Layout.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />,
+      },
+      {
+        path: "/login",
+        element: <UserLogin />,
+      },
+      {
+        path: "/signup",
+        element: <UserRegister />,
+      },
+      {
+        path: "/registergame",
+        element: <GameOperate />,
+      },
+      {
+        path: "/findgames",
+        element: <GamesList />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {/* <App /> */}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
