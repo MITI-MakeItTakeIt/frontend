e709a1c2201e9ed6c62bdd7cc55c9c09775eb99e
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-26 20:38:49 +0900

    ✨feat : header ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,35 @@
+import styled from "styled-components";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <NavigationBar>
+      <NavItems>
+        <li>MITI</li>
+        <li>경기 운영</li>
+        <li>경기 조회</li>
+        <li>로그인</li>
+      </NavItems>
+    </NavigationBar>
+  );
 };
+
+const NavigationBar = styled.header`
+  width: 100vw;
+  height: 4rem;
+
+  display: flex;
+  align-items: center;
+  background-color: #000;
+  color: #fff;
+`;
+const NavItems = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  /* height: 100%; */
+
+  li {
+    list-style: none;
+  }
+`;
