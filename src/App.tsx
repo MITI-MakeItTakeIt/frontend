5c6d6e9d06149c9a3e83378e7612fb2595f13e56
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-26 20:39:05 +0900

    컴포넌트 및 페이지 파일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Header } from "./components/Header";
+import { MainPage } from "./pages/MainPage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <MainPage />
+    </>
+  );
 }
 
 export default App;
