f4c8b37e674f7e621f99c7069513b2363ba18f9b
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-27 23:09:05 +0900

    ✨feat : zod 유효성 검사 툴 추가

--- src/components/UserRegisterForm.tsx
+++ src/components/UserRegisterForm.tsx
@@ -2,10 +2,15 @@ import { useForm } from "react-hook-form";
 import axiosUrl from "../utils/axios";
 import { RegisterField } from "../interface/auth";
 import { userRegisterSchema } from "../modals/userSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 // const { errors } = useFormState(); /
 export const UserRegisterForm = () => {
-  const { register, handleSubmit } = useForm<RegisterField>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterField>({ resolver: zodResolver(userRegisterSchema) });
 
   const onSubmit = async (data: RegisterField) => {
     try {
@@ -25,24 +30,32 @@ export const UserRegisterForm = () => {
           required: true,
         })}
       />
+      {errors.email?.message && <p>{errors.email?.message}</p>}
       <input
         type="password"
         {...register("password", {
           required: true,
         })}
       />
+      {errors.password?.message && <p>{errors.password?.message}</p>}
+
       <input
         type="password"
         {...register("password_check", {
           required: true,
         })}
       />
+      {errors.password_check?.message && (
+        <p>{errors.password_check?.message}</p>
+      )}
+
       <input
         type="text"
         {...register("nickname", {
           required: true,
         })}
       />
+      {errors.nickname?.message && <p>{errors.nickname?.message}</p>}
 
       <button>제출</button>
     </form>
