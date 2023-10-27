0cada50c0e9ce46cfd847b11f2179b3e37e9e71e
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-27 23:36:59 +0900

    ⚙️ refactor : regex 출력 문자 수정

--- src/modals/userSchema.ts
+++ src/modals/userSchema.ts
@@ -3,21 +3,18 @@ import { z } from "zod";
 
 export const userRegisterSchema = z
     .object({
-        email: z.string().email({ message: "Invalid email format" }),
+        email: z.string().email({ message: "이메일 포멧 확인해주세요." }),
+        // 비밀번호는 8자 이상의 영문 대소문자와 숫자, 특수문자를 포함하여야 합니다
         password: z
             .string()
-            .min(8, { message: "Password must be at least 8 characters" }),
+            .trim().regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/, { message: `8자 이상의 영문 대소문자와 숫자, 특수문자를 포함하여야 합니다.` }),
+        // .min(8, { message: "Password must be at least 8 characters" }),
         password_check: z.string(),
-        nickname: z.string().min(5, { message: "Minimum of 5 or more characters" }),
+        nickname: z.string().trim().regex(/^[가-힣a-zA-Z]{2,6}$/, { message: '2자 이상, 6자 이상, 영문 또는 한문 포함하여야 합니다.' }),
+        // .min(5, { message: "Minimum of 5 or more characters" }),
     })
     .refine((data) => data.password === data.password_check, {
-        message: "Password confirmation must match the password",
+        message: "비밀번호가 일치하지 않습니다.",
         path: ["password_check"],
     });
 
-// export const userLoginSchema = z.object({
-//     email: z.string().email({ message: "Invalid email format" }), password: z
-//         .string()
-//         .min(8, { message: "Password must be at least 8 characters" }),
-
-// })
