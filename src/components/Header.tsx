b7062cbdef130e8d6c2273fd9f5adc16db2636bb
Jiwon Lee <giwon.yi339@gmail.com>
2023-11-16 22:02:22 +0900

    🎨 style: 헤더 UI구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,40 @@
 import { NavLink } from "react-router-dom";
+import logo from "../assets/MITI_logo.svg";
 
 export const Header = () => {
   return (
-    <header className="bg-black w-full">
-      <ul className="bg-black ">
-        <NavLink to="/">MITI</NavLink>
-        <NavLink to="/operate-game">경기 운영</NavLink>
-        <NavLink to="/find-games">경기 조회</NavLink>
-        <NavLink to="/login">로그인</NavLink>
-        <NavLink to="/signup">회원가입</NavLink>
-      </ul>
-      <h2 className="bg-black w-full">hello</h2>
+    <header className="flex items-center w-full  max-w-[90] h-[3.75rem] px-[13rem] ">
+      <nav className="flex  items-center justify-between w-full text-[1.25rem] ">
+        <div className="flex items-center jusitfy-evenly gap-[1.5rem]">
+          <NavLink className="" to="/">
+            <img
+              className="w-[5.5rem] h-[1.75rem]"
+              src={logo}
+              alt="miti logo"
+            />
+          </NavLink>
+          <NavLink className="" to="/find-games">
+            <span className="font-bold">빠른 매칭</span>
+          </NavLink>
+          <div
+            className="flex items-center"
+            onClick={() => alert("in progress")}
+          >
+            <span className=" font-bold">팀</span>
+            <span className="text-[0.8125rem] text-[#9B8EFD]">NEW</span>
+          </div>
+        </div>
+        <div className="flex items-center justify-evenly gap-[1.5rem] text-[1rem]">
+          <NavLink to="/login">로그인</NavLink>
+          <NavLink to="/signup">회원가입</NavLink>
+          <NavLink
+            className="bg-[#4065F6] text-white p-[0.5rem] rounded-lg 	"
+            to="/operate-game"
+          >
+            경기 만들기
+          </NavLink>
+        </div>
+      </nav>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {
-      backgroundImage: {},
+      backgroundImage: {
+        logo: "./src/assets/MITI_logo.svg",
+        banner_one: "/src/assets/banner-1.svg",
+        banner_two: "/src/assets/banner-2.svg",
+        game_logo: "/src/assets/game_info.svg",
+      },
     },
   },
   plugins: [],
