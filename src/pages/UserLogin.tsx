87974abc79a74daf542d66217d4ab98ed92fa094
Jiwon Lee <giwon.yi339@gmail.com>
2023-11-19 11:01:47 +0900

    🎨style: 카카오 로그인 버튼 이미지 추가

--- src/pages/UserLogin.tsx
+++ src/pages/UserLogin.tsx
@@ -1,11 +1,15 @@
 import { UserLoginForm } from "../components/UserLoginForm";
+import kakaoLogin from "../assets/kakao_login_medium_wide.png";
 
 export const UserLogin = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen max-w-[90rem] px-[13rem] mx-auto">
+    <div className="flex flex-col items-center mt-[6rem] h-screen max-w-[90rem] px-[13rem] mx-auto">
+      <h1 className="font-bold text-3xl my-[3rem]">로그인</h1>
       <UserLoginForm />
       <hr className="block w-[275px] h-px bg-black my-[3rem]" />
-      <h1>카카오 로그인</h1>
+      <button>
+        <img className="w-[300px]" src={kakaoLogin} alt="kakao login" />
+      </button>
     </div>
   );
 };
