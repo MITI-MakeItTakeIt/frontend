e259d6e4eb27c260d4ff4fb990cb0512fd15b669
Jiwon Lee <giwon.yi339@gmail.com>
2023-11-26 00:19:59 +0900

    ✨feat : 지도 축소/확대 기능

--- src/components/games/KakaoMap.tsx
+++ src/components/games/KakaoMap.tsx
@@ -15,6 +15,10 @@ export const KakaoMap = () => {
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
     const map = new window.kakao.maps.Map(container, options);
+
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성
+    const zoomControl = new window.kakao.maps.ZoomControl();
+    map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
   }, []);
 
   return <div id="map" className="w-full h-full" />;
