08387d35603cd7b367db2be00c2f481010fb86e6
Jiwon Lee <giwon.yi339@gmail.com>
2023-12-08 14:42:31 +0900

    경기 리스트 마커 표시 구현

--- src/components/games/KakaoMap.tsx
+++ src/components/games/KakaoMap.tsx
@@ -6,22 +6,59 @@ declare global {
   }
 }
 
-export const KakaoMap = () => {
+export const KakaoMap = ({ gamesByDateData }) => {
+  console.log("kakao data");
+
+  // console.log(gamesByDateData.data.data);
+  // const gameData = gamesByDateData.data?.data;
+
   useEffect(() => {
     const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
     const options = {
       //지도를 생성할 때 필요한 기본 옵션
       center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 7, //지도의 레벨(확대, 축소 정도)
+      level: 3, //지도의 레벨(확대, 축소 정도)
     };
     const map = new window.kakao.maps.Map(container, options);
 
     // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성
     const zoomControl = new window.kakao.maps.ZoomControl();
     map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+    const geocoder = new window.kakao.maps.services.Geocoder();
 
-    // const gamesData = gamesByDateData?.data?.data;
-  }, []);
+    // if(addressList.length > 0) {}
+    gamesByDateData?.data?.data.map((position) => {
+      return geocoder.addressSearch(
+        // "제주특별자치도 제주시 첨단로 242",
+        position.address,
+        function (result, status) {
+          // 정상적으로 검색이 완료됐으면
+          if (status === window.kakao.maps.services.Status.OK) {
+            const coords = new window.kakao.maps.LatLng(
+              result[0].y,
+              result[0].x
+            );
+
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            const marker = new window.kakao.maps.Marker({
+              map: map,
+              position: coords,
+            });
+
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            const infowindow = new window.kakao.maps.InfoWindow({
+              content:
+                '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>',
+            });
+            infowindow.open(map, marker);
+
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+          }
+        }
+      );
+    });
+  }, [gamesByDateData]);
 
   return <div id="map" className="w-full h-full" />;
 };
